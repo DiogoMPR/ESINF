a1389134a450622cbb4bc5e931d26b118a37d103
Diogo_1220812 <1220812@isep.ipp.pt>
2023-09-21 11:37:05 +0100

    Week 1 pl exercise 1)F done

--- src/main/java/PLs_Week1/Exerc_1F.java
+++ src/main/java/PLs_Week1/Exerc_1F.java
@@ -5,11 +5,37 @@ public class Exerc_1F {
     // To compute the sum of all the elements in an n × n (two-dimensional) array of integers.
 
     public static void main (String[] args){
+        int[][] array1 = {{1, 1}, {1, 1}};
+        int result1 = sumAllElements(array1);
+        System.out.println(result1); // 4
 
+        System.out.println();
+
+        int[][] array2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int result2 = sumAllElements(array2);
+        System.out.println(result2); // 45
      }
 
-     public static int[][] sumOfElements (int n, int[][] array){
+     public static int sumOfElements (int[][] array, int row, int column){
 
-        // Base Case : If the array has only one element, we should return that element
+        // Base Case : the end of the array is reached
+
+         if(row == array.length){
+             return 0;
+
+             // When we reach the end of a column, we should move to the next row
+
+         } else if (column == array[row].length) {
+             return sumOfElements(array, row + 1, 0);
+         } else{
+
+             // Recursive step : Sum the current element and recursively sum the rest of the elements
+
+             int currentElement = array[row][column];
+             return currentElement + sumOfElements(array, row, column + 1);
+         }
+     }
+     public static int sumAllElements (int[][] array){
+        return sumOfElements(array, 0,0);
      }
 }
---
+++ src/main/java/PLs_Week1/Exerc_2.java
@@ -0,0 +1,15 @@
+package PLs_Week1;
+
+public class Exerc_2 {
+        /**
+         *
+         * @param actual the labirinth in its actual (marked) form
+         * @param y coordinate y in the labirinth
+         * @param x coordinate x in the labirinth
+         * @return the marked labirinth or null if there is no way
+         */
+        public static int [][] check(int [][] actual, int y, int x) {
+
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
